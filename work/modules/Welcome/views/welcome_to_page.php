ad551e01746315571b44aff5a5268a55a511be71
Maxcs318 <maxkiller1112@gmail.com>
2019-06-24 14:02:56 +0700

    - -

--- work/modules/Welcome/views/welcome_to_page.php
+++ work/modules/Welcome/views/welcome_to_page.php
@@ -1,19 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Wellcome</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<!-- ajax google -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> 
-</head>
-<body>
 
 <div class="container">
 	<div class="row">
@@ -35,6 +22,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	
 
 </div>
-
-</body>
-</html>
